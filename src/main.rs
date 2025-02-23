de4bb77e3442efe9aaf06e66d137f45dbd253e3c
SithLord34 <singh.shivansh2410@gmail.com>
2025-02-23 13:07:43 +0530

    2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", is_even(21));
+}
+
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
